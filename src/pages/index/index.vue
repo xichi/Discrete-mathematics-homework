ca3db4642d51c3ae9afa62eb849d1b125918b6a0
XiChi <474504964@qq.com>
2019-09-08 13:38:40 +0800

    首页（丑哭了

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+		<li v-for="item in list" :key="item.id" class="exp">{{item.title}}<text>{{item.content}}</text></li>
+		<li class="more"><text>And more...</text></li>
 	</view>
 </template>
 
@@ -11,7 +9,12 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				list:[
+					{title:'实验1',content:'利用真值表求主析取范式和主合取范式的实现'},
+					{title:'实验2',content:'集合上二元关系性质判定的实现'},
+					{title:'实验3',content:'偏序关系中盖住关系的求取及格论中有补格的判定'},
+					{title:'实验4',content:'图的随机生成及欧拉（回）路的确定'},
+				]
 			}
 		},
 		onLoad() {
@@ -23,20 +26,31 @@
 	}
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
+<style lang="less" scoped>
+.content{
+	margin-top:20upx;
+	.exp{
+	   margin:0 auto;
+	   width:95%;
+	   font-size:45upx;
+	   line-height:200upx;
+	   list-style:none;
+	   background-color:#f1f1f1;
+	   box-shadow: 0 0 3upx 0upx #b9b8b8 ;
+	   border-radius:50upx;
+	   color:#105e62;
+	   margin-bottom:20upx;
+	   padding-left:20upx;
+	   text{
+		   font-size:25upx;
+		   color:#428377; 
+		   padding-left:30upx;
+	   }
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
+	.more{
+	   list-style:none;
+	   text-align:center;
 	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+}
+   
 </style>
